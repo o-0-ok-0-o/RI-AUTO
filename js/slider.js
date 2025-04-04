f67e716eb36f59dcd2f92b64d69807c277a11f5d
o-0-ok-0-o <azattnurr79@gmail.com>
2025-04-04 21:26:17 +0300

    11123

---
+++ js/slider.js
@@ -0,0 +1,20 @@
+const menuItems = document.querySelectorAll(".menu-item");
+const slider = document.getElementById("slider");
+const slides = document.querySelectorAll(".slide");
+
+menuItems[0].classList.add("active")
+
+menuItems.forEach((item) => {
+  item.addEventListener("mouseenter", () => {
+    const index = +item.dataset.slide;
+    slides[index].scrollIntoView({ behavior: "smooth" });
+    menuItems.forEach((i) => i.classList.remove("active"));
+    item.classList.add("active");
+  });
+});
+
+slider.addEventListener("scroll", () => {
+  const index = Math.round(slider.scrollTop / window.innerHeight);
+  menuItems.forEach((i) => i.classList.remove("active"));
+  menuItems[index].classList.add("active");
+});
